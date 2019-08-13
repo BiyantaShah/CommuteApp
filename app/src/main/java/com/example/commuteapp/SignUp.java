a1bc7589b3f513b7ae7c436c4855d66aa9c356ad
Biyanta <biyanta.shah@citrix.com>
2019-08-12 21:23:48 -0400

    Sign up corrected - added validations

--- app/src/main/java/com/example/commuteapp/SignUp.java
+++ app/src/main/java/com/example/commuteapp/SignUp.java
@@ -1,5 +1,6 @@
 package com.example.commuteapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,8 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -46,31 +51,44 @@ public class SignUp extends AppCompatActivity {
                 final String name = edName.getText().toString();
                 final String password = edPwd.getText().toString();
                 final String verifyPass = edPwd2.getText().toString();
-                String emailid = edEmail.getText().toString();
+                final String emailid = edEmail.getText().toString();
                 final String homeaddress = edAddress.getText().toString();
                 final String phone = edphone.getText().toString();
 
-                ProfileValue profileValue = new ProfileValue();
-                profileValue.setuserName(name);
-                profileValue.setuserAddress(homeaddress);
-                profileValue.setuserEmail(emailid);
-                profileValue.setuserPhone(phone);
-
-                final Map<String, Object> dataMap = new HashMap<String, Object>();
-
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("Users");
-//                myRef.setValue("Hello, World!tttttttttttttttttttt");
-
-                String[] email = emailid.split("@");
-                if (email.length < 2) {
+                // check for none of the fields to be empty
+                if (name.isEmpty() || password.isEmpty() || verifyPass.isEmpty() ||emailid.isEmpty() || homeaddress.isEmpty() || phone.isEmpty()) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
-                    builder.setMessage("Please enter a valid email id")
+                    builder.setMessage("Please enter values in all the fields")
                             .setNegativeButton("Retry", null)
                             .create()
                             .show();
-                } // if valid email id then check if its a citrix id
-                else if(!email[1].equals("citrix.com")) {
+                    return;
+                }
+
+
+                // password and verify password should be same
+                if (!password.equals(verifyPass)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
+                    builder.setMessage("Both passwords should match")
+                            .setNegativeButton("Retry", null)
+                            .create()
+                            .show();
+                    return;
+                }
+
+                //check if email id is present in correct format.
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                final DatabaseReference myRef = database.getReference("Users");
+                String[] email = emailid.split("@");
+                if (!emailid.contains("@")) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
+                    builder.setMessage("Please enter a valid Citrix given email id")
+                            .setNegativeButton("Retry", null)
+                            .create()
+                            .show();
+                    return;
+                } // correct email id and now if its valid
+                else if (!email[1].equals("citrix.com")) {
 
                     AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
                     builder.setMessage("Please enter your citrix email id")
@@ -78,21 +96,84 @@ public class SignUp extends AppCompatActivity {
                             .create()
                             .show();
 
-                } // if both conditions are true for email go on to add that in the database
-                else {
+                    return;
 
-                    dataMap.put(email[0], profileValue.toMap());
-                    myRef.updateChildren(dataMap);
-                    session = new Session(getApplicationContext());
-                    session.setusername(name);
-                    session.setuserEmail(emailid);
-                    session.setuserAddress(homeaddress);
-                    session.setuserPhone(phone);
-
-
-                    Intent intent = new Intent(SignUp.this, Home.class);
-                    startActivity(intent);
                 }
+
+
+                //check if we already have this user registered
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        boolean userExists = false;
+                        if (dataSnapshot.exists()) {
+                            GenericTypeIndicator<Map<String, Object>> genericTypeIndicator = new GenericTypeIndicator<Map<String, Object>>() {
+                            };
+                            Map<String, Object> dataMap = dataSnapshot.getValue(genericTypeIndicator);
+
+
+                            for (String key : dataMap.keySet()) {
+                                Object data = dataMap.get(key);
+
+                                try {
+                                    HashMap<String, Object> userData = (HashMap<String, Object>) data;
+                                    String emails = (String) userData.get("userEmail");
+
+                                    System.out.println("emails from db" + emails + "email given" + emailid );
+                                    // check if user exists
+                                    if (emails.equals(emailid)) {
+                                        System.out.println("comeshgdhfdgjhkgh");
+                                        userExists = true;
+                                    }
+
+                                } catch (ClassCastException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+
+                        }
+
+                        if (userExists) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
+                            builder.setMessage("We have an account with this email ID, please login to the app")
+                                    .setNegativeButton("Retry", null)
+                                    .create()
+                                    .show();
+
+                            return;
+                        }
+                        else {
+                            ProfileValue profileValue = new ProfileValue();
+                            profileValue.setuserName(name);
+                            profileValue.setuserAddress(homeaddress);
+                            profileValue.setuserEmail(emailid);
+                            profileValue.setuserPhone(phone);
+
+                            final Map<String, Object> dataMap = new HashMap<String, Object>();
+                            String[] email = emailid.split("@");
+                            
+                            // all validations are done so it is safe to put the data into the database
+                            dataMap.put(email[0], profileValue.toMap());
+                            myRef.updateChildren(dataMap);
+                            session = new Session(getApplicationContext());
+                            session.setusername(name);
+                            session.setuserEmail(emailid);
+                            session.setuserAddress(homeaddress);
+                            session.setuserPhone(phone);
+
+
+                            Intent intent = new Intent(SignUp.this, Home.class);
+                            startActivity(intent);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
             }
         });
     }
